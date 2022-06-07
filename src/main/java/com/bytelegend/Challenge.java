134f9b793803413149f56c81e68d18c264e40096
zhyz0727 <zhyz0727@163.com>
2022-06-07 04:38:03 +0000

    Update at 2022-06-07T04:38:03.521640Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -15,6 +15,18 @@ public class Challenge {
      * <p>If the given string is a symmetric string, then return `true`; otherwise, return `false`.
      */
     public static boolean isSymmetric(String str) {
-        return false;
+        char[] chars = str.toCharArray();
+        int i = 0;
+        int j = chars.length;
+        boolean res = true;
+        do {
+            if (chars[i] == chars[j - 1]) {
+                i++;
+                j--;
+            } else {
+                res = false;
+            }
+        } while (i != j && (i + 1) != j && res);
+        return res;
     }
 }
