4d2544aeda21ed188d9194f3c154db5c7d02aa40
amine01laaboudi <al2000229@gmail.com>
2022-07-20 20:46:30 +0000

    Update at 2022-07-20T20:46:30.900033Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -17,10 +17,9 @@ public class Challenge {
     public static boolean isSymmetric(String str) {
         int i = 0, j = str.length() - 1;
         while (i < j) {
-            if (str.charAt(i) != str.charAt(j)) {
+            if (str.charAt(i++) != str.charAt(j--)) {
                 return false;
             }
-            ++i,++j;
         }
         return true;
     }
